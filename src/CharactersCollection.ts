a13ade7f4be70f2632684f2fb773e388680c7a1c
Kayla Webb <kaylarwebb@gmail.com>
2021-01-31 19:11:34 -0500

    implement sorter for numbers and strings

---
+++ src/CharactersCollection.ts
@@ -0,0 +1,22 @@
+export class CharactersCollection {
+  constructor(public data: string) {}
+
+  get length(): number {
+    return this.data.length;
+  }
+
+  compare(leftIdx: number, rightIdx: number): boolean {
+    return this.data[leftIdx].toLowerCase() > this.data[rightIdx].toLowerCase();
+  }
+
+  swap(leftIdx: number, rightIdx: number): void {
+    // convert the string of characters into array
+    const characters = this.data.split('');
+    // swap
+    const leftHand = characters[leftIdx];
+    characters[leftIdx] = characters[rightIdx];
+    characters[rightIdx] = leftHand;
+    // join array back together
+    this.data = characters.join('');
+  }
+}
